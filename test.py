a40fdaa834ad3862f5af41a34ee5bf7b198a2c1e
josephdickson11 <josepholadipupoibeun@gmail.com>
2022-08-03 17:14:49 +0100

    update the dummy code again

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
 print(f"this is a new {2022} format")
 
-day = int(input("please, input the number of days: "))
-SECS_IN_DAYS = 60 * 60
-MINS_IN_DAYS = 60
 
-print(f"total number of secs in {day} days is {SECS_IN_DAYS * day}")
+
+
+def day_in_sec(day):
+    SECS_IN_DAYS = 60 * 60
+
+    result = day * SECS_IN_DAYS
+
+    return f"total number of secs in {day} days is {result} seconds"
+
+def day_in_mins(day):
+    MINS_IN_DAYS = 60
+
+    result = day * MINS_IN_DAYS
+
+    return f"total number of minutes in {day} days is {result} minutes"
+
+day = int(input("please, input the number of days: "))
+
+print(day_in_mins(day))
+
+print(day_in_sec(day))
