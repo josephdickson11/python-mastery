89165f431368c0f9055cb83dc100dcbe80ee12a2
josephdickson11 <josepholadipupoibeun@gmail.com>
2022-08-03 16:55:22 +0100

    update dummy code

---
+++ test.py
@@ -0,0 +1,7 @@
+print(f"this is a new {2022} format")
+
+day = int(input("please, input the number of days: "))
+SECS_IN_DAYS = 60 * 60
+MINS_IN_DAYS = 60
+
+print(f"total number of secs in {day} days is {SECS_IN_DAYS * day}")
